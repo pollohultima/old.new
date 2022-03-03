b27dc5503e7bfd46e311b631000cc94522034174
pollohultima <matteo.polonio@gmail.com>
2022-03-03 17:19:52 +0100

    routes fiel defined

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+    {
+    path: '/contacts',
+    name: 'Contacts',
+    component: () => import('../views/Contacts.vue')
+  },
+  {
+    path: '/roadmap',
+    name: 'Roadmap',
+    component: () => import('../views/Roadmap.vue')
+  },
+  {
+    path: '/nftgallery',
+    name: 'NftGallery',
+    component: () => import('../views/NftGallery.vue')
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
